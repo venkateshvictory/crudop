826a308a9943d46a05fd6672092f7ccf05801019
venkateshvictory <doddivenktesh768@gmail.com>
2023-09-17 01:08:45 -0700

    secondcommit

---
+++ src/main/java/com/trainee/crud/controller/HeoConotooler.java
@@ -0,0 +1,16 @@
+package com.trainee.crud.controller;
+
+import org.aspectj.apache.bcel.generic.ReturnaddressType;
+
+public class HeoConotooler {
+
+	public static int add(int a,int b)
+	{
+		return a+b;
+	}
+	public static void main(String[] args) {
+		
+		add(5,6);
+	
+}}
+
---
+++ src/main/java/com/trainee/crud/controller/Mulcontrolelr.java
@@ -0,0 +1,12 @@
+package com.trainee.crud.controller;
+
+public class Mulcontrolelr {
+
+static int mult(int a,int b) {
+	System.out.println();
+	return a*b;
+}
+	public static void main(String[] args) {
+		mult(3,5);
+	}
+}
