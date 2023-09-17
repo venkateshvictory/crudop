c070e0dee0b0f9d027609b8ec15608fe92361c10
venkateshvictory <doddivenktesh768@gmail.com>
2023-09-17 00:58:10 -0700

    first_commit

---
+++ src/main/java/com/trainee/crud/controller/GitController.java
@@ -0,0 +1,8 @@
+package com.trainee.crud.controller;
+
+public class GitController {
+
+	String m2() {
+		return "sumanth";
+	}
+}
---
+++ src/main/java/com/trainee/crud/controller/HellloController.java
@@ -0,0 +1,9 @@
+package com.trainee.crud.controller;
+
+public class HellloController {
+
+	public static void m1() {
+	
+		System.out.println("hello heroooo");
+	}
+}
